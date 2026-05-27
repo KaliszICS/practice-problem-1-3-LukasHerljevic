904a53c413ccd5f1807fa7ab47f4834f38e3a213
LukasHerljevic <258844296+LukasHerljevic@users.noreply.github.com>
2026-05-27 14:26:45 +0000

    e

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
+        
+    }
 
-	}
+    public static String readFile(String filename) {
+        StringBuilder sb = new StringBuilder();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {}
+        
+        return sb.toString();
+    }
 
-	
-
+    public static String backwardsReadFile(String filename) {
+        String content = readFile(filename);
+        StringBuilder sb = new StringBuilder(content);
+        return sb.reverse().toString();
+    }
 }
+
